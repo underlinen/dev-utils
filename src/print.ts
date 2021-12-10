ed3aaef05597a843bae0e71f115f83aca150a347
shanejix <shanejix@hotmail.com>
2021-12-10 17:45:43 +0800

    core: swtich to typescript

---
+++ src/print.ts
@@ -0,0 +1,30 @@
+import chalk from 'chalk';
+
+const print = {
+  info(...args) {
+    const prefix = chalk.blue(args[0]);
+    args.shift();
+    args.unshift(prefix);
+    console.log.apply(console, args);
+  },
+  success(...args) {
+    const prefix = chalk.green(args[0]);
+    args.shift();
+    args.unshift(prefix);
+    console.log.apply(console, args);
+  },
+  warn(...args) {
+    const prefix = chalk.yellow(args[0]);
+    args.shift();
+    args.unshift(prefix);
+    console.log.apply(console, args);
+  },
+  error(...args) {
+    const prefix = chalk.red(args[0]);
+    args.shift();
+    args.unshift(prefix);
+    console.log.apply(console, args);
+  },
+};
+
+export default print;
